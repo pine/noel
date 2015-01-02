6827d07ec8b65f61d8378ed64721b3af147699ea
MIZUNE Pine <pinemz@gmail.com>
2015-01-02 22:00:05 +0900

    Add checking ketarin

--- env.go
+++ env.go
@@ -7,9 +7,15 @@ import (
 
 func checkEnv() error {
     fmt.Println("# checking choco")
-    _, err := exec.LookPath("choco")
+    if _, err := exec.LookPath("choco"); err != nil {
+        return err
+    }
     
-    if err != nil {
+    fmt.Println("# checking ketarin")
+    if _, err := exec.LookPath("ketarin"); err != nil {
+        fmt.Print("\n")
+        fmt.Println("Try to install used by chocolatey as following:")
+        fmt.Println("$ choco install chocolateypackageupdater")
         return err
     }
     
