810371163fc011dd96fae27dfb45bf10352e72ee
Pine Mizune <pinemz@gmail.com>
2015-01-05 00:03:28 +0900

    Add rev

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -4,11 +4,13 @@ sudo apt-get install zip
 zip noel.zip noel.exe
 
 GIT_BRANCH=`git name-rev --name-only HEAD`
+GIT_REV=`git rev-parse --short HEAD`
+
 mkdir dist
 mv noel.exe dist/noel_$GIT_BRANCH.exe
 mv noel.zip dist/noel_$GIT_BRANCH.zip
 
 if [ "$GIT_BRANCH" = "master" ]; then
 	go get github.com/tcnksm/ghr
-	ghr --username $GITHUB_USERNAME --token $GITHUB_TOKEN --replace --prerelease --debug auto-release-$GIT_BRANCH dist/
+	ghr --username $GITHUB_USERNAME --token $GITHUB_TOKEN --replace --prerelease --debug auto-release-$GIT_BRANCH-$GIT_REV dist/
 fi
