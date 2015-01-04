c5abbbec442f19d21217fbda1652391f0d3bac39
Pine Mizune <pinemz@gmail.com>
2015-01-04 23:49:07 +0900

    Fix bug

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -3,11 +3,11 @@
 sudo apt-get install zip
 zip noel.zip noel.exe
 
+GIT_BRANCH=`git name-rev --name-only HEAD`
 mkdir dist
 mv noel.zip dist/noel_$GIT_BRANCH.zip
 
 if [ "$GIT_BRANCH" = "master" ]; then
-	GIT_BRANCH=`git name-rev --name-only HEAD`
 	go get github.com/tcnksm/ghr
 	ghr --username $GITHUB_USERNAME --token $GITHUB_TOKEN --replace --prerelease --debug auto-release-$GIT_BRANCH dist/
 fi
